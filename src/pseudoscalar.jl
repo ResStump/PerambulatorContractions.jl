b22358a9b355cf3db60510de52fef1d5694f7cf6
stumpa01 <stumpa@student.ethz.ch>
2024-02-20 13:00:08 +0100

    Add increase_separation function

--- src/pseudoscalar.jl
+++ src/pseudoscalar.jl
@@ -13,6 +13,7 @@
 import MKL
 import LinearAlgebra as LA
 import TensorOperations as TO
+import Random
 import TOML
 import HDF5
 import DelimitedFiles
@@ -24,7 +25,7 @@ include("IO.jl")
 include("contractions.jl")
 
 # Add infile manually to arguments
-# pushfirst!(ARGS, "-i", "run_pseudoscalar/input/pseudoscalar_B450r000.toml")
+# pushfirst!(ARGS, "-i", "run_pseudoscalar/input/pseudoscalar_16x8v1.toml")
 
 
 # %%###############
@@ -58,68 +59,59 @@ sparse_modes_file(n_cnfg) = parms.sparse_modes_dir/
                             "sparse_modes_$(parms_toml["Run name"]["name"])n$(n_cnfg)"
 
 
-
-
-
-# Compare imported mode doublets to those computed from Laplacian eigenmodes
-############################################################################
-
-# Read mode doublets
-#= Φ_klt_iₚ = read_mode_doublets(mode_doublets_file(1), 2)[:, :, :, iₚ]
-
-
-# Read eigen modese and the positions
-sparse_modes_arrays = read_sparse_modes(sparse_modes_file(1), 2)
-x_sink_μiₓ, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt = sparse_modes_arrays
-
-x_sink_μiₓ
-x_src_μiₓt
-v_sink_ciₓkt
-v_src_ciₓkt
-
-
-# Number of lattice points
-_, N_points = size(x_sink_μiₓ)
-
-
-# Create empty mode doublet Array
-Φ_klt_iₚ_ = Array{ComplexF64, 3}(undef, parms.N_modes, parms.N_modes, parms.Nₜ)
-Φ_klt_iₚ_adj = Array{ComplexF64, 3}(undef, parms.N_modes, parms.N_modes, parms.Nₜ)
-
-for t in 1:parms.Nₜ
-    # Laplace modes and mode doublets at time t
-    v_sink_ciₓk_t = @view v_sink_ciₓkt[:, :, :, t]
-    Φ_kl_tiₚ = @view Φ_klt_iₚ[:, :, t]
-    Φ_kl_tiₚ_ = @view Φ_klt_iₚ_[:, :, t]
-    Φ_kl_tiₚ_adj = @view Φ_klt_iₚ_adj[:, :, t]
-
-    # Compute exp(±ipx) and reshape it to match shape of v_sink_ciₓk_t
-    exp_mipx_sink_iₓ = exp.(-2π*im*(x_sink_μiₓ./parms.Nₖ)'*p)
-    exp_mipx_sink_iₓ = reshape(exp_mipx_sink_iₓ, (1, N_points, 1))
-
-    TO.@tensoropt begin
-        Φ_kl_tiₚ_[k, l] = conj(v_sink_ciₓk_t[c, iₓ, k]) * 
-                          (exp_mipx_sink_iₓ .* v_sink_ciₓk_t)[c, iₓ, l]
-    end
-
-    TO.@tensoropt begin
-        Φ_kl_tiₚ_adj[k, l] = (v_sink_ciₓk_t[c, iₓ, l]) * 
-                          conj((exp_mipx_sink_iₓ) .* v_sink_ciₓk_t)[c, iₓ, k]
-    end
-
-    @assert Φ_kl_tiₚ' ≈ Φ_kl_tiₚ_adj
-end
-
-Φ_klt_iₚ ≈ Φ_klt_iₚ_
-Φ_klt_iₚ ≈ Φ_klt_iₚ_adj =#
-
-
-
-# Now follows the actual computation
 # %%
 
+function increase_separation(sparse_modes_arrays, N_sep_new, n_cnfg)
+    if mod(N_sep_new, 2) != 0
+        throw(ArgumentError("'N_sep_new' has to be a multiple of 2."))
+    end
+    x_sink_μiₓ, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt = sparse_modes_arrays
+
+    # Determine seperation in current sparse modes
+    N_sep = (prod(parms.Nₖ)/size(x_sink_μiₓ)[2])^(1/3)
+    N_sep = round(Int, N_sep)
+
+    # Create array with indices of new sparse modes
+    iₓ_arr = collect(1:size(x_sink_μiₓ)[2])
+    Nₖ_sparse = round(Int, length(iₓ_arr)^(1/3))
+    iₓ_arr = reshape(iₓ_arr, (Nₖ_sparse, Nₖ_sparse, Nₖ_sparse))
+    iₓ_arr = permutedims(iₓ_arr, (3, 2, 1))
+    division = N_sep_new÷N_sep
+    if division == 0
+        throw(ArgumentError("'N_sep_new' has to be bigger than the N_sep in "*
+                            "sparse_modes_arrays."))
+    end
+
+    seed = parms_toml["Increased Separation"]["seed"]
+    seed_cnfg = seed ⊻ n_cnfg
+    rng = Random.MersenneTwister(seed_cnfg)
+    
+    iₓ_sink_new_arr = vec(iₓ_arr[1:division:end, 1:division:end, 1:division:end])
+    iₓ_src_new_arr = Array{Int}(undef, length(iₓ_sink_new_arr), parms.Nₜ)
+    for iₜ in 1:parms.Nₜ
+        offset = rand(rng, 1:division, 3)
+        iₓ_src_new_arr[:, iₜ] = vec(iₓ_arr[offset[1]:division:end,
+                                           offset[2]:division:end,
+                                           offset[3]:division:end])
+    end
+
+    # Create new sparse spaces/modes at sink
+    x_sink_new_μiₓ = x_sink_μiₓ[:, iₓ_sink_new_arr]
+    v_sink_new_ciₓkt = v_sink_ciₓkt[:, iₓ_sink_new_arr, :, :]
+
+    # Create new sparse spaces/modes at sink
+    x_src_new_μiₓt = Array{Int}(undef, size(x_sink_new_μiₓ)..., parms.Nₜ)
+    v_src_new_ciₓkt = Array{ComplexF64}(undef, size(x_sink_new_μiₓ)..., parms.N_modes, parms.Nₜ)
+    for iₜ in 1:parms.Nₜ
+        x_src_new_μiₓt[:, :, iₜ] = x_src_μiₓt[:, iₓ_src_new_arr[:, iₜ], iₜ]
+        v_src_new_ciₓkt[:, :, :, iₜ] = v_src_ciₓkt[:, iₓ_src_new_arr[:, iₜ], :, iₜ]
+    end
+
+    return x_sink_new_μiₓ, x_src_new_μiₓt, v_sink_new_ciₓkt, v_src_new_ciₓkt
+end
 
 
+# %%
 
 # Get momentum index
 p_arr = read_mode_doublet_momenta(mode_doublets_file(parms.cnfg_indices[1]))
@@ -138,6 +130,11 @@ for (i_cnfg, n_cnfg) in enumerate(parms.cnfg_indices)
     @time "Finished configuration $n_cnfg" begin
         @time "  Read sparse modes " begin
             sparse_modes_arrays = read_sparse_modes(sparse_modes_file(n_cnfg))
+            if parms_toml["Increased Separation"]["increase_sep"]
+                N_sep_new = parms_toml["Increased Separation"]["N_sep_new"]
+                sparse_modes_arrays = increase_separation(sparse_modes_arrays, N_sep_new,
+                                                          n_cnfg)
+            end
         end
         @time "  Read mode doublets" begin
             Φ_kltiₚ = read_mode_doublets(mode_doublets_file(n_cnfg))
@@ -191,9 +188,10 @@ end
 
 # %%
 
-#= #= correlator = HDF5.h5read("run_pseudoscalar_juwels_tmp0/program_files/results/B450r000_32modes_pseudoscalar.hdf5", "Correlator")
-correlator2 = HDF5.h5read("run_pseudoscalar_juwels_tmp0/program_files/results/B450r000_32modes_pseudoscalar_p0.hdf5", "Correlator")
-correlator3 = HDF5.h5read("run_pseudoscalar_juwels_tmp0/program_files/results/B450r000_32modes_pseudoscalar_sparse.hdf5", "Correlator") =#
+#= #= path = "/home/stumpa/Seafile/Dokumente/HU_Berlin,DESY/Programs/wit_-_MainzLattice/PerambulatorContractions/run_pseudoscalar_juwels_tmp0/program_files/results_(p0,0,0)"
+correlator = HDF5.h5read("$path/B450r000_32modes_pseudoscalar.hdf5", "Correlator")
+correlator2 = HDF5.h5read("$path/B450r000_32modes_pseudoscalar_p0.hdf5", "Correlator")
+correlator3 = HDF5.h5read("$path/B450r000_32modes_pseudoscalar_sparse.hdf5", "Correlator") =#
 
 Nₜ, _, _ = size(correlator)
 
@@ -219,17 +217,17 @@ corr2_[corr2_.<=0] .= NaN
 
 plot = Plt.plot(xlabel=L"t/a", ylabel=L"C(t)", yscale=:log)
 Plt.scatter!(1:Nₜ, corr, label="Using mode doublets")
-#Plt.scatter!(1:Nₜ, corr2, label="For zero momentum")
+# Plt.scatter!(1:Nₜ, corr2, label="For zero momentum")
 Plt.scatter!(1:Nₜ, corr3, label="Position space sampling")
-for i in 1:parms.N_cnfg
-    #= Plt.plot!(legend=false)
+#= for i in 1:parms.N_cnfg
+    Plt.plot!(legend=false)
     Plt.scatter!(1:Nₜ, corr_[:, 1, i][corr_[:, 1, i].>0.0], label="Using mode doublets")
-    #Plt.scatter!(1:Nₜ, corr2_[:, 1, i][corr2_[:, 1, i].>0.0], label="Using full eigenvectors") =#
+    #Plt.scatter!(1:Nₜ, corr2_[:, 1, i][corr2_[:, 1, i].>0.0], label="Using full eigenvectors")
 
-end
-display(plot)
+end =#
+display(plot) =#
 
-# Plt.savefig(p, "pseudoscalar_p1,0,0_Nsep1.pdf") =#
+# Plt.savefig(p, "pseudoscalar_p1,0,0_Nsep1.pdf")
 
 
 # %%
