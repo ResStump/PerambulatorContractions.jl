0e2833624a464e2561fd50cca612707a12d9aa7e
stumpa01 <stumpa@student.ethz.ch>
2024-02-29 14:52:48 +0100

    Add allocate_arrays.jl

---
+++ src/allocate_arrays.jl
@@ -0,0 +1,60 @@
+import HDF5
+
+
+@doc raw"""
+    allocate_perambulator() -> τ_αkβlt
+
+Allocate the empty array `τ_αkβlt` with the correct size to store a perambulator.
+"""
+function allocate_perambulator()
+    τ_αkβlt = Array{ComplexF64}(undef, 4, parms.N_modes, 4, parms.N_modes, parms.Nₜ)
+
+    return τ_αkβlt
+end
+
+@doc raw"""
+    allocate_mode_doublets(mode_doublets_file) -> Φ_kltiₚ
+
+Allocate the empty array `Φ_kltiₚ` with the correct size to store mode doublets.
+
+The HDF5 file path `mode_doublets_file` is used to get the number of momenta stored in the
+mode doublets.
+"""
+function allocate_mode_doublets(mode_doublets_file)
+    # Get number of momenta in mode doublets
+    Nₚ, _ = size(read_mode_doublet_momenta(mode_doublets_file))
+
+    # Allocate array
+    Φ_kltiₚ = Array{ComplexF64}(undef, parms.N_modes, parms.N_modes, parms.Nₜ, Nₚ)
+
+    return Φ_kltiₚ
+end
+
+@doc raw"""
+    allocate_sparse_modes(sparse_modes_file=nothing; N_points=nothing)
+        -> x_sink_μiₓ, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt
+
+Allocate the empty arrays `x_sink_μiₓ`, `x_src_μiₓt`, `v_sink_ciₓkt`, `v_src_ciₓkt` with
+the correct size to store the sparse spaces and modes.
+
+The number of points in the sparse modes are either determined from the HDF5 file
+`sparse_modes_file` or take as `N_points`.
+"""
+function allocate_sparse_modes(sparse_modes_file=nothing; N_points=nothing)
+    if !isnothing(sparse_modes_file)
+        # If sparse_modes_file given get number of points in sparse space from it
+        hdf5_file = HDF5.h5open(string(sparse_modes_file), "r")
+        _, N_points = size(hdf5_file["sparse_space_sink"])
+        close(hdf5_file)
+    elseif isnothing(N_points)
+        throw(ArgumentError("either sparse_modes_file or N_points have to be given."))
+    end
+
+    # Allocate arrays
+    x_sink_μiₓ = Array{Int32}(undef, 3, N_points)
+    x_src_μiₓt = Array{Int32}(undef, 3, N_points, parms.Nₜ)
+    v_sink_ciₓkt = Array{ComplexF64}(undef, 3, N_points, parms.N_modes, parms.Nₜ)
+    v_src_ciₓkt = Array{ComplexF64}(undef, 3, N_points, parms.N_modes, parms.Nₜ)
+
+    return x_sink_μiₓ, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt
+end
