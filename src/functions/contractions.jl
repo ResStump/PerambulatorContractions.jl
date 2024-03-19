48973dfeef9cc99c49f27155136da2788918d4e2
stumpa01 <stumpa@student.ethz.ch>
2024-03-19 16:35:14 +0100

    Add pseudoscalar contractions for two different perambulators

--- src/functions/contractions.jl
+++ src/functions/contractions.jl
@@ -1,21 +1,27 @@
 @doc raw"""
+    pseudoscalar_contraction_p0!(Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray, t₀::Integer)
     pseudoscalar_contraction_p0!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray, t₀::Integer)
 
-Contract the perambulator 'τ\_αkβlt' to get the pseudoscalar correlator and 
-store it in 'Cₜ'. The source time 't₀' is used to circularly shift 'Cₜ' such that the
-source time is at the origin.
+Contract the perambulators `τ₁_αkβlt` and `τ₂_αkβlt` to get the pseudoscalar correlator
+for zero momentum where `τ₁_αkβlt` is used to propagate in forward and `τ₂_αkβlt` in
+backward direction. If only one perambulator `τ_αkβlt` is given, it is used to propagete in
+both directions.
+
+The result is store it in `Cₜ`. The source time `t₀` is used to circularly shift `Cₜ` such
+that the source time is at the origin.
 """
 function pseudoscalar_contraction_p0!(
-    Cₜ::AbstractVector, τ_αkβlt::AbstractArray, t₀::Integer
+    Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray, t₀::Integer
 )
     # Loop over all sink time indice
     for iₜ in 1:parms.Nₜ
-        # Perambulator at sink (index 'iₜ')
-        τ_αkβl_t = @view τ_αkβlt[:, :, :, :, iₜ]
+        # perambulators at sink (index `iₜ`)
+        τ₁_αkβl_t = @view τ₁_αkβlt[:, :, :, :, iₜ]
+        τ₂_αkβl_t = @view τ₂_αkβlt[:, :, :, :, iₜ]
 
         # Tensor contraction
         TO.@tensoropt begin
-            C = τ_αkβl_t[α, k, β, l] * conj(τ_αkβl_t[α, k, β, l])
+            C = τ₁_αkβl_t[α, k, β, l] * conj(τ₂_αkβl_t[α, k, β, l])
         end
 
         # Circularly shift time such that t₀=0
@@ -24,36 +30,43 @@ function pseudoscalar_contraction_p0!(
 
     return
 end
+pseudoscalar_contraction_p0!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray, t₀::Integer) =
+    pseudoscalar_contraction_p0!(Cₜ, τ_αkβlt, τ_αkβlt, t₀)
 
 @doc raw"""
-    pseudoscalar_contraction!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray,
-                              Φ_kltiₚ::AbstractArray,  t₀::Integer, iₚ::Integer)
+    pseudoscalar_contraction!(Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray, Φ_kltiₚ::AbstractArray, t₀::Integer, iₚ::Integer)
+    pseudoscalar_contraction!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray, Φ_kltiₚ::AbstractArray, t₀::Integer, iₚ::Integer)
 
-Contract the perambulator 'τ\_αkβlt' and the mode doublet 'Φ\_kltiₚ' to get the
-pseudoscalar correlator and store it in 'Cₜ'. The source time 't₀' is used to circularly
-shift 'Cₜ' such that the source time is at the origin. The index 'iₚ' sets the momentum
-that is used from the mode doublet.
+Contract the perambulators `τ₁_αkβlt` and `τ₂_αkβlt` and the mode doublets `Φ_kltiₚ` to get
+the pseudoscalar correlator where `τ₁_αkβlt` is used to propagate in forward and `τ₂_αkβlt`
+in backward direction. If only one perambulator `τ_αkβlt` is given, it is used to propagete
+in both directions.
+
+The result is store it in `Cₜ`. The source time `t₀` is used to circularly shift `Cₜ` such
+that the source time is at the origin. The index `iₚ` sets the momentum that is used from
+the mode doublets.
 """
 function pseudoscalar_contraction!(
-    Cₜ::AbstractVector, τ_αkβlt::AbstractArray, Φ_kltiₚ::AbstractArray,
-    t₀::Integer, iₚ::Integer
+    Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray,
+    Φ_kltiₚ::AbstractArray, t₀::Integer, iₚ::Integer
 )
-    # Index for source time 't₀'
+    # Index for source time `t₀`
     i_t₀ = mod1(t₀+1, parms.Nₜ)
 
-    # Mode doublet at source time 't₀'
+    # Mode doublet at source time `t₀`
     Φ_kl_t₀iₚ = @view Φ_kltiₚ[:, :, i_t₀, iₚ]
 
     # Loop over all sink time indice
     for iₜ in 1:parms.Nₜ
-        # Mode doublet and perambulator at sink time t (index 'iₜ')
+        # Mode doublet and perambulators at sink time t (index `iₜ`)
         Φ_kl_tiₚ = @view Φ_kltiₚ[:, :, iₜ, iₚ]
-        τ_αkβl_t = @view τ_αkβlt[:, :, :, :,iₜ]
+        τ₁_αkβl_t = @view τ₁_αkβlt[:, :, :, :,iₜ]
+        τ₂_αkβl_t = @view τ₂_αkβlt[:, :, :, :,iₜ]
         
         # Tensor contraction
         TO.@tensoropt begin
-            C = Φ_kl_tiₚ[k, k'] * τ_αkβl_t[α, k', β, l'] *
-                conj(Φ_kl_t₀iₚ[l, l']) * conj(τ_αkβl_t[α, k, β, l])
+            C = Φ_kl_tiₚ[k, k'] * τ₁_αkβl_t[α, k', β, l'] *
+                conj(Φ_kl_t₀iₚ[l, l']) * conj(τ₂_αkβl_t[α, k, β, l])
         end
 
         # Circularly shift time such that t₀=0
@@ -62,31 +75,36 @@ function pseudoscalar_contraction!(
 
     return
 end
+pseudoscalar_contraction!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray,
+                          Φ_kltiₚ::AbstractArray, t₀::Integer, iₚ::Integer) = 
+    pseudoscalar_contraction!(Cₜ, τ_αkβlt, τ_αkβlt, Φ_kltiₚ, t₀, iₚ)
 
 @doc raw"""
-    pseudoscalar_sparse_contraction!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray,
-                                     sparse_modes_arrays::NTuple{4, AbstractArray},
-                                     t₀::Integer, p::Vector)
+    pseudoscalar_sparse_contraction!(Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray, sparse_modes_arrays::NTuple{4, AbstractArray}, t₀::Integer, p::Vector)
+    pseudoscalar_sparse_contraction!(Cₜ::AbstractVector, τ_αkβlt::AbstractArray, sparse_modes_arrays::NTuple{4, AbstractArray}, t₀::Integer, p::AbstractVector)
 
-Contract the perambulator 'τ\_αkβlt' and the sparse Laplace modes stored in
-'sparse\_modes\_arrays' to get the pseudoscalar correlator and store it in 'Cₜ'. The source
-time 't₀' is used to circularly shift 'Cₜ' such that the source time is at the origin.
-The array 'p' is the integer momentum that is used for the momentum projection of the
-correlator.
+Contract the perambulators `τ₁_αkβlt` and `τ₂_αkβlt` and the sparse Laplace modes stored in
+`sparse_modes_arrays` to get the pseudoscalar correlator where `τ₁_αkβlt` is used to
+propagate in forward and `τ₂_αkβlt` in backward direction. If only one perambulator
+`τ_αkβlt` is given, it is used to propagete in both directions.
+
+The result is store it in `Cₜ`. The source time `t₀` is used to circularly shift `Cₜ` such
+that the source time is at the origin. The array `p` is the integer momentum that is used
+for the momentum projection of the correlator.
 """
 function pseudoscalar_sparse_contraction!(
-    Cₜ::AbstractVector, τ_αkβlt::AbstractArray,
+    Cₜ::AbstractVector, τ₁_αkβlt::AbstractArray, τ₂_αkβlt::AbstractArray,
     sparse_modes_arrays::NTuple{4, AbstractArray}, t₀::Integer, p::AbstractVector
 )
     x_sink_μiₓ, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt = sparse_modes_arrays
 
-    # Index for source time 't₀'
+    # Index for source time `t₀`
     i_t₀ = t₀+1
     
     # Number of points on spares lattice
     _, N_points = size(x_sink_μiₓ)
 
-    # Source position and Laplace modes at source time 't₀'
+    # Source position and Laplace modes at source time `t₀`
     x_src_μiₓ_t₀ = @view x_src_μiₓt[:, :, i_t₀]
     v_src_ciₓk_t₀ = @view v_src_ciₓkt[:, :, :, i_t₀]
 
@@ -98,18 +116,19 @@ function pseudoscalar_sparse_contraction!(
     
     # Loop over all sink time indice
     for iₜ in 1:parms.Nₜ
-        # Perambulator and Laplace modes at sink time t (index 'iₜ')
-        τ_αkβl_t = @view τ_αkβlt[:, :, :, :, iₜ]
+        # Perambulators and Laplace modes at sink time t (index `iₜ`)
+        τ₁_αkβl_t = @view τ₁_αkβlt[:, :, :, :,iₜ]
+        τ₂_αkβl_t = @view τ₂_αkβlt[:, :, :, :,iₜ]
         v_sink_ciₓk_t = @view v_sink_ciₓkt[:, :, :, iₜ]
 
         # Tensor contraction
         TO.@tensoropt begin
             C = conj(v_sink_ciₓk_t[a, iₓ', k]) * 
                 (exp_mipx_sink_iₓ .* v_sink_ciₓk_t)[a, iₓ', k'] *
-                τ_αkβl_t[α, k', β, l'] *
+                τ₁_αkβl_t[α, k', β, l'] *
                 conj(v_src_ciₓk_t₀[b, iₓ, l']) *
                 (exp_ipx_src_iₓ .* v_src_ciₓk_t₀)[b, iₓ, l] *
-                conj(τ_αkβl_t[α, k, β, l])
+                conj(τ₂_αkβl_t[α, k, β, l])
         end
 
         # Normalization
@@ -121,3 +140,7 @@ function pseudoscalar_sparse_contraction!(
 
     return
 end
+pseudoscalar_sparse_contraction!(
+    Cₜ::AbstractVector, τ_αkβlt::AbstractArray,
+    sparse_modes_arrays::NTuple{4, AbstractArray}, t₀::Integer, p::AbstractVector
+) = pseudoscalar_sparse_contraction!(Cₜ, τ_αkβlt, τ_αkβlt, sparse_modes_arrays, t₀, p)
