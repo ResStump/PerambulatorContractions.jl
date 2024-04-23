ef2d2f8e896c70edfbd6d5c98eb4be6121745b56
stumpa01 <stumpa@student.ethz.ch>
2024-04-23 15:26:54 +0200

    Make gamma matrices complex

--- src/functions/variables.jl
+++ src/functions/variables.jl
@@ -12,16 +12,16 @@ O = [0 0;
 σ = Dict(0=>σ₀, 1=>σ₁, 2=>σ₂, 3=>σ₃)
 
 # Euclidean gamma matrice in chiral rep
-γ₁ = [   O -im*σ₁;
-      im*σ₁    O]
-γ₂ = [   O -im*σ₂;
-      im*σ₂    O]
-γ₃ = [   O -im*σ₃;
-      im*σ₃    O]
-γ₄ = [ O  -σ₀;
-      -σ₀  O]
-γ₅ = [σ₀  O;
-      O  -σ₀]
+γ₁ = ComplexF64[   O -im*σ₁;
+                im*σ₁    O]
+γ₂ = ComplexF64[   O -im*σ₂;
+                im*σ₂    O]
+γ₃ = ComplexF64[   O -im*σ₃;
+                im*σ₃    O]
+γ₄ = ComplexF64[ O  -σ₀;
+                -σ₀  O]
+γ₅ = ComplexF64[σ₀  O;
+                O  -σ₀]
 
 γ = [γ₁, γ₂, γ₃, γ₄, γ₅]
 
