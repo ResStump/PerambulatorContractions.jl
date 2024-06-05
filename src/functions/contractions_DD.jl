5aeafcc47162e31a39675a6d4544476664f89ebf
stumpa01 <stumpa@student.ethz.ch>
2024-06-05 08:59:53 +0200

    Add contractions_DD.jl with a local DD contraction function

---
+++ src/functions/contractions_DD.jl
@@ -0,0 +1,114 @@
+########################
+# Locale DD Contractions
+########################
+
+@doc raw"""
+    DD_local_contractons!(C_tnmn̄m̄iₚ::AbstractArray, τ_charm_αkβlt::AbstractArray, τ_light_αkβlt::AbstractArray, sparse_modes_arrays::NTuple{4, AbstractArray}, Γ_arr::AbstractVector{<:AbstractMatrix}, t₀::Integer, p_arr::AbstractVector{<:AbstractVector}
+
+Contract the charm perambulator `τ_charm_αkβlt` and the light perambulator `τ_light_αkβlt`
+and the sparse Laplace modes stored in `sparse_modes_arrays` to get the local DD
+correlator. The matrices in `Γ_arr` are the matrices in the interpolating operators.
+The correlator is computed for all possible combinations of them. This gives a vacuum expectation value of the form \
+`<(ūΓ₁c d̄Γ₂c)(x) (c̄Γbar₃u c̄Γbar₄d)(0)>` \
+(in position space). The result is stored in `C_tnmn̄m̄iₚ` where the indices n, m, n̄, m̄
+correspond to the indices of the Γ's in the expectation value in the given order.
+
+The source time `t₀` is used to circularly shift `C_tnmn̄m̄iₚ` such that the source time is at
+the origin. The array `p_arr` contains the integer momenta the correlator is projected to
+(index iₚ in `C_tnmn̄m̄iₚ`).
+"""
+function DD_local_contractons!(
+    C_tnmn̄m̄iₚ::AbstractArray, τ_charm_αkβlt::AbstractArray, τ_light_αkβlt::AbstractArray,
+    sparse_modes_arrays::NTuple{4, AbstractArray}, Γ_arr::AbstractVector{<:AbstractMatrix},
+    t₀::Integer, p_arr::AbstractVector{<:AbstractVector}
+)
+    # Unpack sparse modes arrays
+    x_sink_μiₓt, x_src_μiₓt, v_sink_ciₓkt, v_src_ciₓkt = sparse_modes_arrays
+
+    # Index for source time `t₀`
+    i_t₀ = t₀+1
+
+    # Number of points on spares lattice
+    _, N_points, _ = size(x_sink_μiₓt)
+
+    # Convert vector of γ-matrices to contiguous array and compute Γbar matrices
+    Γ_αβn = stack(Γ_arr)
+    TO.@tensoropt Γbar_αβn[α, β, n] := γ[4][α, α'] * conj(Γ_αβn)[β', α', n] * γ[4][β', β]
+
+    # Set correlator C_tnmn̄m̄iₚ to zero
+    C_tnmn̄m̄iₚ .= 0
+
+    # Loop over all sink time indice
+    for iₜ in 1:parms.Nₜ
+        # Perambulators, sink position and Laplace modes at sink time t (index `iₜ`)
+        τ_charm_αkβl_t = @view τ_charm_αkβlt[:, :, :, :, iₜ]
+        τ_light_αkβl_t = @view τ_light_αkβlt[:, :, :, :, iₜ]
+
+        # Conjugate perambulator and multiply γ₅ to use γ₅-hermiticity
+        TO.@tensoropt (l, k) begin
+            γ₅τ′γ₅_light_αkβl_t[α, k, β, l] :=
+                γ[5][α, α'] * conj(τ_light_αkβl_t)[α', k, β', l] * γ[5][β', β]
+        end
+
+        # Loop over sink position iₓ′ and source position iₓ
+        # (using multithreading in inner for loop)
+        for iₓ′ in 1:N_points
+        Threads.@threads for iₓ in 1:N_points
+            # Laplace modes at sink time t (index `iₜ`) and position iₓ′
+            v_sink_ck_iₓ′t = @view v_sink_ciₓkt[:, iₓ′, :, iₜ]
+
+            # Laplace modes at src time t₀ and position iₓ
+            v_src_ck_iₓt₀ = @view v_src_ciₓkt[:, iₓ, :, i_t₀]
+
+            # Tensor contractions
+            #####################
+
+            # Disconnected part
+            TO.@tensoropt (l, k, l', k') begin
+                C_disc_nn̄[n, n̄] :=
+                    conj(v_sink_ck_iₓ′t)[a, k] * v_sink_ck_iₓ′t[a, k'] *
+                    Γ_αβn[α, α', n] * τ_charm_αkβl_t[α', k', β, l'] *
+                    conj(v_src_ck_iₓt₀)[b, l'] * v_src_ck_iₓt₀[b, l] *
+                    Γbar_αβn[β, β', n̄] * γ₅τ′γ₅_light_αkβl_t[α, k, β', l]
+            end
+
+            # Connected part
+            TO.@tensoropt (l, k, l', k', l̃, k̃, l̃', k̃') begin
+                C_conn_nmn̄m̄[n, m, n̄, m̄] :=
+                    conj(v_sink_ck_iₓ′t)[a, k] * v_sink_ck_iₓ′t[a, k'] *
+                    Γ_αβn[α, α', n] * τ_charm_αkβl_t[α', k', β, l'] *
+                    conj(v_src_ck_iₓt₀)[b, l'] * v_src_ck_iₓt₀[b, l] *
+                    Γbar_αβn[β, β', m̄] * γ₅τ′γ₅_light_αkβl_t[α_, k̃, β', l] *
+                    conj(v_sink_ck_iₓ′t)[ã, k̃] * v_sink_ck_iₓ′t[ã, k̃'] *
+                    Γ_αβn[α_, α_', m] * τ_charm_αkβl_t[α_', k̃', β_, l̃'] *
+                    conj(v_src_ck_iₓt₀)[b̃, l̃'] * v_src_ck_iₓt₀[b̃, l̃] *
+                    Γbar_αβn[β_, β_', n̄] * γ₅τ′γ₅_light_αkβl_t[α, k, β_', l̃]
+            end
+
+            # Combine connected and disconnected part
+            TO.@tensoropt begin
+                C_nmn̄m̄[n, m, n̄, m̄] :=
+                    C_disc_nn̄[n, n̄]*C_disc_nn̄[m, m̄] - C_conn_nmn̄m̄[n, m, n̄, m̄]
+            end
+
+            # Momentum projection
+            m2πiΔx = -2π*im * 
+                (x_sink_μiₓt[:, iₓ′, iₜ] - x_src_μiₓt[:, iₓ, i_t₀])./parms.Nₖ
+            i_Δt = mod1(iₜ-t₀, parms.Nₜ)
+            for (iₚ, p) in enumerate(p_arr)
+                # Circularly shift time with Δt such that t₀=0
+                C_nmn̄m̄_Δtiₚ = @view C_tnmn̄m̄iₚ[i_Δt, :, :, :, :, iₚ]
+                TO.@tensoropt begin
+                    C_nmn̄m̄_Δtiₚ[n, m, n̄, m̄] += (exp(m2πiΔx'*p)) * C_nmn̄m̄[n, m, n̄, m̄]
+                end
+            end
+        end
+        end
+    end
+
+    # Normalization
+    C_tnmn̄m̄iₚ *= (prod(parms.Nₖ)/N_points)^2
+
+    return
+end
+
