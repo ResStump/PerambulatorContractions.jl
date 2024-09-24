65175bab94a53eafc18c82b590cddca6c02594b1
stumpa01 <stumpa@student.ethz.ch>
2024-09-24 17:56:15 +0200

    Update PerembulatorContractions module file

--- src/PerambulatorContractions.jl
+++ src/PerambulatorContractions.jl
@@ -9,11 +9,13 @@ import HDF5
 import DelimitedFiles
 import FilePathsBase: /, Path
 import Combinatorics as Comb
+import Dates
 import PrecompileTools
 
 include("functions/allocate_arrays.jl")
 include("functions/contractions_meson.jl")
 include("functions/contractions_DD.jl")
+include("functions/contractions_dad.jl")
 include("functions/IO.jl")
 include("functions/mpi_utils.jl")
 include("functions/utils.jl")
