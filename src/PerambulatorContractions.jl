03326e139628dfccaa9de81e1cd71f0e31626bf2
stumpa01 <stumpa@student.ethz.ch>
2025-03-06 15:52:03 +0100

    Update PerambulatorContractions.jl

--- src/PerambulatorContractions.jl
+++ src/PerambulatorContractions.jl
@@ -3,6 +3,7 @@ module PerambulatorContractions
 import MKL
 import LinearAlgebra as LA
 import TensorOperations as TO
+import ITensors as IT
 import MPI
 import TOML
 import HDF5
@@ -10,6 +11,7 @@ import DelimitedFiles
 import FilePathsBase: /, Path
 import ArgParse as AP
 import Combinatorics as Comb
+import Symbolics
 import Dates
 import PrecompileTools
 
@@ -23,8 +25,8 @@ include("functions/contractions_dad.jl")
 include("functions/contractions_DD-dad_mixed.jl")
 include("functions/IO.jl")
 include("functions/mpi_utils.jl")
-include("functions/utils.jl")
 include("functions/variables.jl")
+include("functions/utils.jl")
 
 # Precompile module
 include("functions/precompilation.jl")
